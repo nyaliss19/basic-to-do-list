998c8b1cd4a3bc52846c3f4ca0e019541d85c2aa
nyaliss19 <53007681+nyaliss19@users.noreply.github.com>
2022-04-13 20:32:50 -0400

    Changed event listener for deleteButton
    Changed event listener to listen only for clicks on the button itself not the whole item

--- js/script.js
+++ js/script.js
@@ -12,6 +12,8 @@ function newItem() {
   let deleteButton = $("<crossOutButton></crossOutButton>");
   deleteButton.append(document.createTextNode("X"));
   li.append(deleteButton);
+
+  deleteButton.on("click", function deleteListItem() {
     li.addClass("delete");
   }),
     $("#list").sortable();
